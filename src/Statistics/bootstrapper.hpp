5d9fd5677f2616c050ef98cd10f10c6b6d294eec
W. Joe Meese <meese022@umn.edu>
2021-09-22 10:35:16 -0500

    Append include directories for the project so header installation works well

--- src/Statistics/bootstrapper.hpp
+++ src/Statistics/bootstrapper.hpp
@@ -2,5 +2,18 @@
 #define DATA_ANALYSIS_BOOTSTRAPPER
 // This is the base Bootstrapper class
 // that takes in a Datastream
+#include <DataStream/datastream.hpp>
+
+namespace Statistics
+{
+    template<typename input_t, typename output_t = input_t>
+    class Bootstrapper
+    {
+        public:
+
+        private:
+
+    };
+}
 
 #endif
